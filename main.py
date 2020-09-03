dc7b5e971818a8cca46b040be14d97a6cdd2bfcc
jln5438 <jln5438@psu.edu>
2020-09-03 23:12:31 +0000

    got rid of lower case

--- main.py
+++ main.py
@@ -2,61 +2,61 @@
 firstGrade=input("Enter your course 1 letter grade: ")
 firstCredit=input("Enter your course 1 credit: ")
 firstGPA=0.0
-if(firstGrade=='A' or firstGrade=='a'):
+if(firstGrade=='A'):
   firstGPA=4.0
-elif(firstGrade=='A-' or firstGrade=='a-'):
+elif(firstGrade=='A-'):
   firstGPA=3.67
-elif(firstGrade=='B+' or firstGrade=='b+'):
+elif(firstGrade=='B+'):
   firstGPA=3.33
-elif(firstGrade=='B' or firstGrade=='b'):
+elif(firstGrade=='B'):
   firstGPA=3.0
-elif(firstGrade=='B-' or firstGrade=='b-'):
+elif(firstGrade=='B-'):
   firstGPA=2.67
-elif(firstGrade=='C+' or firstGrade=='c+'):
+elif(firstGrade=='C+'):
   firstGPA=2.33
-elif(firstGrade=='C' or firstGrade=='c'):
+elif(firstGrade=='C'):
   firstGPA=2.0
-elif(firstGrade=='D' or firstGrade=='d'):
+elif(firstGrade=='D'):
   firstGPA=1.0
 print("Grade point for course 1 is: "+str(firstGPA))
 secondGrade=input("Enter your course 2 letter grade: ")
 secondCredit=input("Enter your course 2 credit: ")
 secondGPA=0.0
-if(secondGrade=='A' or secondGrade=='a'):
+if(secondGrade=='A'):
   secondGPA=4.0
-elif(secondGrade=='A-' or secondGrade=='a-'):
+elif(secondGrade=='A-'):
   secondGPA=3.67
-elif(secondGrade=='B+' or secondGrade=='b+'):
+elif(secondGrade=='B+'):
   secondGPA=3.33
-elif(secondGrade=='B' or secondGrade=='b'):
+elif(secondGrade=='B'):
   secondGPA=3.0
-elif(secondGrade=='B-' or secondGrade=='b-'):
+elif(secondGrade=='B-'):
   secondGPA=2.67
-elif(secondGrade=='C+' or secondGrade=='c+'):
+elif(secondGrade=='C+'):
   secondGPA=2.33
-elif(secondGrade=='C' or secondGrade=='c'):
+elif(secondGrade=='C'):
   secondGPA=2.0
-elif(secondGrade=='D' or secondGrade=='d'):
+elif(secondGrade=='D'):
   secondGPA=1.0
 print("Grade point for course 2 is: "+str(secondGPA))
 thirdGrade=input("Enter your course 3 letter grade: ")
 thirdCredit=input("Enter your course 3 credit: ")
 thirdGPA=0.0
-if(thirdGrade=='A' or thirdGrade=='a'):
+if(thirdGrade=='A'):
   thirdGPA=4.0
-elif(thirdGrade=='A-' or thirdGrade=='a-'):
+elif(thirdGrade=='A-'):
   thirdGPA=3.67
-elif(thirdGrade=='B+' or thirdGrade=='b+'):
+elif(thirdGrade=='B+'):
   thirdGPA=3.33
-elif(thirdGrade=='B' or thirdGrade=='b'):
+elif(thirdGrade=='B'):
   thirdGPA=3.0
-elif(thirdGrade=='B-' or thirdGrade=='b-'):
+elif(thirdGrade=='B-'):
   thirdGPA=2.67
-elif(thirdGrade=='C+' or thirdGrade=='c+'):
+elif(thirdGrade=='C+'):
   thirdGPA=2.33
-elif(thirdGrade=='C' or thirdGrade=='c'):
+elif(thirdGrade=='C'):
   thirdGPA=2.0
-elif(thirdGrade=='D' or thirdGrade=='d'):
+elif(thirdGrade=='D'):
   thirdGPA=1.0
 print("Grade point for course 3 is: "+str(thirdGPA))
 
